0d8aad4c42e4bfa62c259b5bf24c0f9bf22f3e0f
yingchi.pei <yingchi.pei@gmail.com>
2018-09-09 16:14:25 +0800

    add new mock data

--- js/experiment.js
+++ js/experiment.js
@@ -1,10 +1,10 @@
 'use strict';
 
 // Location of data files
-const trialsFile = "./data/experiments.csv"
-const menuL1File = "./data/menu_depth_1.csv"
-const menuL2File = "./data/menu_depth_2.csv"
-const menuL3File = "./data/menu_depth_3.csv"
+const trialsFile = "./data/experiments_breadth_1.csv"
+const menuL1File = "./data/menu_depth_1_breadth_1.csv"
+const menuL2File = "./data/menu_depth_2_breadth_1.csv"
+const menuL3File = "./data/menu_depth_3_breadth_1.csv"
 
 // Global variables
 var menu;
